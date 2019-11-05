7f0c3a8f3ba678bcd92ce78fd62a9189100c06b5
yejin <yejin3122@naver.com>
2019-11-06 02:37:41 +0900

    add similarity

--- src/service/mainService.js
+++ src/service/mainService.js
@@ -22,6 +22,36 @@ function getDistance(mVec, sVec) {
     return sum;
 }
 
+function getSimilarity(mVec, sVec) {
+    let sumM = 0;
+    let sumS = 0;
+    let mul = [];
+
+    // matrix multiple
+    for(let i=0; i<mVec.length; i++){
+        var row = [];
+        for(let j=0; j<mVec.length; j++){
+            let x = 0;
+            for(let k=0; k<mVec.length; k++) {
+                if(sVec[i] != null)
+                    x += mVec[i][k]*sVec[k][j];
+            }
+            row.push(x);
+        }
+        mul.push(row);
+    }
+
+    for (let i=0; i<mVec.length; i++) {
+        sumM += mVec[i]**2;
+    }
+
+    for (let i=0; i<sVec.length; i++) {
+        sumM += sVec[i]**2;
+    }
+
+    return mul / (Math.sqrt(sumM) * Math.sqrt(sumS));
+}
+
 async function postCoord(rssi) {
     // get Server's BSSID 8
     let serverBssid = ['06:09:b4:76:cc:ac',
@@ -64,14 +94,20 @@ async function postCoord(rssi) {
         // get only ap{n} value by list
         let sRssi = getListByRow(serverRssi[i], apNum);
 
-        // compare distance
-        let dst = getDistance(mRssi, sRssi);
+        // calculate cosine similarity
+        let sim = getSimilarity(mRssi, sRssi);
 
-        if (dst < min || i == 0) {
-            min = dst;
-            mX = serverRssi[i].x;
-            mY = serverRssi[i].y;
+        if(sim >= 0.5) {
+            // compare distance
+            let dst = getDistance(mRssi, sRssi);
+
+            if (dst < min || i == 0) {
+                min = dst;
+                mX = serverRssi[i].x;
+                mY = serverRssi[i].y;
+            }
         }
+        
     }
 
     return {x: mX, y: mY};
